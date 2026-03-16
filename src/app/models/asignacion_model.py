4cae2fca18037083efd8baf4d50684979f79d0bc
Dairo Carrasquilla <dcarrasquilla@ises.com.co>
2026-03-16 14:52:55 -0500

    chore: separación de logica

--- src/app/models/asignacion_model.py
+++ src/app/models/asignacion_model.py
@@ -1,4 +1,4 @@
-from fastapi import UploadFile, status, BackgroundTasks
+from fastapi import UploadFile, status, BackgroundTasks, Response
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 
@@ -13,60 +13,7 @@ from io import BytesIO
 
 import polars as pl
 
-async def task(file: UploadFile, session: Session) -> None:
-    file = await file.read()
-    file_buffer = BytesIO(file)
-    df_pmu = pl.read_excel(file_buffer)
-
-    logger.info("Archivo excel leido ✔️")
-
-    # step 1: Limpiar la tabla con registros actuales.
-
-    stmt = text("""
-        UPDATE pmu_priorizacion_cliente
-        SET eliminado = :eliminado
-        WHERE fecha_registro::DATE = CURRENT_DATE;
-    """)
-
-    session.execute(stmt, [{"eliminado": True}])
-    session.commit()
-
-    logger.info("PMU eliminada ✔️")
-
-    # step 2: normalizar columnas e insertar en la tabla.
-
-    map_columns = {
-        "Prioridad": "prioridad",
-        "Cuenta": "cuenta",
-        "Numero de la Os": "numero_os",
-        "Tipo_Orden": "tipo_orden",
-        "Tipo_Brigada": "tipo_brigada",
-        "Tipo_Actividad": "tipo_actividad",
-        "Fecha_Asignación_Cliente": "fecha_asignacion_cliente"
-    }
-
-    df_pmu = df_pmu.rename(map_columns)
-
-    df_pmu = df_pmu.with_columns(
-        pl.col("prioridad").fill_nan(None),
-        pl.col("cuenta").fill_nan(None),
-        pl.col("numero_os").fill_nan(None),
-        pl.col("tipo_orden").str.strip_chars().str.to_lowercase(),
-        pl.col("tipo_brigada").str.strip_chars().str.to_lowercase().str.normalize("NFD").str.replace(r"[\u0300-\u036f]", ""),
-        pl.col("tipo_actividad").str.strip_chars().str.to_lowercase().str.normalize("NFD").str.replace(r"[\u0300-\u036f]", "")
-    )
-
-    df_pmu.write_database(
-        table_name="pmu_priorizacion_cliente",
-        connection=database.db_uri,
-        engine=database.db_engine,
-        if_table_exists="append"
-    )
-
-    logger.info("PMU registrado. ✔️")
-
-    # step 3: Procesar Maestro para el cruze con el PMU
-
+def actualizar_maestro(session:Session) -> Response:
     stmt = text("""
         SELECT md.id, md.nic, md.priorizacion
         FROM maestro_db md
@@ -121,21 +68,79 @@ async def task(file: UploadFile, session: Session) -> None:
             session.commit()
 
             logger.info("Tabla temp maestro eliminada ✔️")
+
+            return JSONResponse(
+                content=jsonable_encoder(
+                    {"status": "procesado"}
+                ),
+                status_code=status.HTTP_200_OK
+            )
         except Exception as e:
             session.rollback()
             logger.error(f"Error: actualización asignacion: {e}")
 
-        return None
-
     logger.info("Maestro pendiente. ✖️")
-    return None
-
-async def registra_pmu(file: UploadFile, session: Annotated[Session, Depends(database.get_session)], bgtask: BackgroundTasks):
-    bgtask.add_task(task, file, session)
 
     return JSONResponse(
         content=jsonable_encoder(
-            {"status": "procesando"}
+            {"status": "pendiente"}
         ),
         status_code=status.HTTP_200_OK
     )
+
+async def registra_pmu(file: UploadFile, session: Annotated[Session, Depends(database.get_session)]):
+    file = await file.read()
+    file_buffer = BytesIO(file)
+    df_pmu = pl.read_excel(file_buffer)
+
+    logger.info("Archivo excel leido ✔️")
+
+    # step 1: Limpiar la tabla con registros actuales.
+
+    stmt = text("""
+        UPDATE pmu_priorizacion_cliente
+        SET eliminado = :eliminado
+        WHERE fecha_registro::DATE = CURRENT_DATE;
+    """)
+
+    session.execute(stmt, [{"eliminado": True}])
+    session.commit()
+
+    logger.info("PMU eliminada ✔️")
+
+    # step 2: normalizar columnas e insertar en la tabla.
+
+    map_columns = {
+        "Prioridad": "prioridad",
+        "Cuenta": "cuenta",
+        "Numero de la Os": "numero_os",
+        "Tipo_Orden": "tipo_orden",
+        "Tipo_Brigada": "tipo_brigada",
+        "Tipo_Actividad": "tipo_actividad",
+        "Fecha_Asignación_Cliente": "fecha_asignacion_cliente"
+    }
+
+    df_pmu = df_pmu.rename(map_columns)
+
+    df_pmu = df_pmu.with_columns(
+        pl.col("prioridad").fill_nan(None),
+        pl.col("cuenta").fill_nan(None),
+        pl.col("numero_os").fill_nan(None),
+        pl.col("tipo_orden").str.strip_chars().str.to_lowercase(),
+        pl.col("tipo_brigada").str.strip_chars().str.to_lowercase().str.normalize("NFD").str.replace(r"[\u0300-\u036f]", ""),
+        pl.col("tipo_actividad").str.strip_chars().str.to_lowercase().str.normalize("NFD").str.replace(r"[\u0300-\u036f]", "")
+    )
+
+    df_pmu.write_database(
+        table_name="pmu_priorizacion_cliente",
+        connection=database.db_uri,
+        engine=database.db_engine,
+        if_table_exists="append"
+    )
+
+    logger.info("PMU registrado. ✔️")
+
+    # step 3: Procesar Maestro para el cruze con el PMU
+
+    return actualizar_maestro(session)
+
